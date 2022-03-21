f6c940447932d038d889a2b88a512df916f231df
aje1018 <aje1018@wildcats.unh.edu>
2022-03-21 15:45:09 -0400

    HOPEFULLY fixed 500 internal error

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.secret_key = "IAN"
 def home():
     return render_template('home.html')
 
-@app.route("/courses")
-def courses():
-    return render_template('courses.html')
+@app.route("/documents")
+def documents():
+    return render_template('documents.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
